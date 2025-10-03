2b306f0f0ba1e4f63c96f290fd3de87f15418005
markos <marko.sterk@izum.si>
2025-10-03 07:35:37 +0200

    PyJoltTestClient bug fix - add lifespan events

--- src/pyjolt/testing/pyjolt_test_client.py
+++ src/pyjolt/testing/pyjolt_test_client.py
@@ -2,6 +2,7 @@
 Test client class
 """
 from typing import TYPE_CHECKING
+from asgi_lifespan import LifespanManager
 from httpx import AsyncClient, ASGITransport
 
 if TYPE_CHECKING:
@@ -11,22 +12,39 @@ class PyJoltTestClient:
     """
     Test client class for testing of PyJolt applications
     """
-    def __init__(self, app: "PyJolt"):
+    def __init__(self, app: "PyJolt", base_url: str = "http://testserver"):
         self.app = app
-        self.transport = ASGITransport(app=self.app)
-        self.client = AsyncClient(transport=self.transport, base_url="http://testserver")
+        self._lifespan: LifespanManager | None = None
+        self._transport: ASGITransport | None = None
+        self.client: AsyncClient | None = None
+        self.base_url: str = base_url
 
     async def __aenter__(self):
-        # __aenter__ can be empty or do any setup
+        #Starts up app with lifespan events (triggers startup methods)
+        self._lifespan = LifespanManager(self.app)
+        await self._lifespan.__aenter__()
+
+        self._transport = ASGITransport(app=self.app)
+        self.client = AsyncClient(transport=self._transport, base_url=self.base_url)
         return self
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
-        # Properly closes the underlying client, triggering lifespan.shutdown
-        await self.client.aclose()
+        # Closes the underlying HTTPX client
+        if self.client is not None:
+            await self.client.aclose()
+
+        # Executes app shutdown with lifespan events (triggers shutdown methods)
+        if self._lifespan is not None:
+            await self._lifespan.__aexit__(exc_type, exc_val, exc_tb)
+
+        self.client = None
+        self._transport = None
+        self._lifespan = None
 
     async def request(self, method: str, path: str, **kwargs):
-        response = await self.client.request(method, path, **kwargs)
-        return response
+        if self.client is None:
+            raise RuntimeError("PyJoltTestClient must be used as an async context manager.")
+        return await self.client.request(method, path, **kwargs)
 
     async def get(self, path: str, **kwargs):
         return await self.request("GET", path, **kwargs)
