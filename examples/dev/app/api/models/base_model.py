fac3ef353b915636d4462f1b079d3792cfa35684
MarkoSterk <marko_sterk@hotmail.com>
2025-10-31 10:33:18 +0100

    Base model change for database.sql

--- examples/dev/app/api/models/base_model.py
+++ examples/dev/app/api/models/base_model.py
@@ -1,12 +1,12 @@
 """
 Base model
 """
-from sqlalchemy.orm import mapped_column, Mapped, DeclarativeBase
 from typing import Type
+from sqlalchemy.orm import mapped_column, Mapped
 
-from pyjolt.database.sql import create_declarative_base
+from pyjolt.database.sql import create_declarative_base, DeclarativeBaseModel
 
-Base: Type[DeclarativeBase] = create_declarative_base("db")
+Base: Type[DeclarativeBaseModel] = create_declarative_base("db")
 
 class BaseModel(Base):
 
